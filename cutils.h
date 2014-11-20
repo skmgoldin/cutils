fd957b9bd37c4e0cb43f7b1b6319b74d4b2cfdef
Mike Goldin <mag2277@columbia.edu>
2014-11-19 22:44:00 -0500

    Whoops. Forgot the header.

--- cutils.h
+++ cutils.h
@@ -8,4 +8,5 @@ int flushstream(FILE *stream);
 char * jsURLparser(int argc, char **argv);
 void print(char *s);
 char * tokenizer(char *cpy, char *tok, int which, const char *delim);
+int filesize(FILE *fp);
 #endif
